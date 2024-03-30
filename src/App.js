276483aede0ba654ce9bdd0a83d1e2b4992fff76
sharmaparag076 <sharmaparag076@gmail.com>
2024-03-31 01:42:15 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'; // Import useState from React
 import './App.css';
-import IMG from "./logo.svg"
+import giff from "./Assets/gig.jpg"
 function App() {
+  const [yes, setYes] = useState(false);
+  const [double, setDouble] = useState(false);
 
   const handleNoHover = () => {
-    var x = document.getElementById('no-btn');
+    const x = document.getElementById('no-btn');
     x.style.position = "absolute";
     x.style.top = Math.random() * 90 + "vh";
     x.style.left = Math.random() * 90 + "vw";
-  }
+  };
+
   const handleYes = () => {
-    document.getElementsByTagName('h1')[0].textContent = "Congratulations,Now Can i call you are mine ?";
-    console.log("yes");
-  }
+    if (yes) {
+      handleDoubleYes();
+    } else {
+      const h1Element = document.getElementsByTagName('h1')[0];
+      h1Element.textContent = "Congratulations, Now can I call you mine?";
+      setYes(true);
+    }
+  };
+
+  const handleDoubleYes = () => {
+    const h1Element = document.getElementsByTagName('h1')[0];
+    h1Element.textContent = "😍";
+    setDouble(true);
+  };
+
   return (
     <>
       <div className='cute-gif'>
-        <img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExcWNhdHUxZzRzdDNkbWdibWF3eTVqdnczYXV4NjBsYzMxbnF5cXlpMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3oEjI4sFlp73fvEYgw/giphy.gif" />
+
+        {!double && < img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExcWNhdHUxZzRzdDNkbWdibWF3eTVqdnczYXV4NjBsYzMxbnF5cXlpMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3oEjI4sFlp73fvEYgw/giphy.gif" alt="cute gif" />
+        }
+        {double && < img src={giff} alt="cute gif" />
+        }
         <h1>Main Flirt krna shuru Krun ?</h1>
-        <div>
-          <button onClick={handleYes}>YES</button>
-          <button id="no-btn" onMouseEnter={handleNoHover}>NO</button>
-        </div>
+        {!double && (
+          <div>
+            <button onClick={handleYes}>YES</button>
+            <button id="no-btn" onMouseEnter={handleNoHover}>NO</button>
+          </div>
+        )}
       </div>
     </>
   );
